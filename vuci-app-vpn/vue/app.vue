b558e292bef1d6f4dbabefd17244c5a32ba9325c
Naglis <84019338+Naglisimo@users.noreply.github.com>
2022-07-08 15:54:38 +0300

    Add files via upload

---
+++ vuci-app-vpn/vue/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <button @click="openModal(true)"></button>
+    <edit-modal isVisible="isVisible"/>
+  </div>
+</template>
+
+<script>
+import EditModal from './components/EditModal.vue'
+export default {
+components: {
+  EditModal
+},
+  data () {
+    return {
+      title: 'OpenVPN',
+      isVisible: false
+    }
+  },
+  methods: {
+    openModal (boolean) {
+      this.isVisible = boolean
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuci-app-vpn/vue/components/EditModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <a-modal :visible="isVisible">
+    <h1>{{title}}</h1>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditModal',
+  props: ['isVisible'],
+  data () {
+    return {
+      title: 'modal from component'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
